87509649839bb4ae097faa22474b81c162350b5a
정구아 <wjdrndk96@gmail.com>
2022-02-04 21:23:56 +0900

    [BE]refac(room): 날라간 거 복구

--- backend/src/main/java/com/ssafy/api/room/controller/RoomController.java
+++ backend/src/main/java/com/ssafy/api/room/controller/RoomController.java
@@ -6,12 +6,19 @@ import com.ssafy.api.room.dto.request.RoomUserRequest;
 import com.ssafy.api.room.dto.response.RoomUserResponse;
 import com.ssafy.api.room.service.RoomService;
 import com.ssafy.common.model.response.BaseResponseBody;
+import com.ssafy.domain.bookmark.entity.Bookmark;
+import com.ssafy.domain.bookmark.repository.BookmarkRepository;
 import com.ssafy.domain.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -37,5 +44,29 @@ public class RoomController {
     ResponseEntity<List<RoomUserResponse>> getUserList(@PathVariable("room_seq") Long room_seq){
         List<RoomUserResponse> response = roomService.getUserList(room_seq);
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/ban")
+    ResponseEntity<? extends BaseResponseBody> ban(@Auth User user, @RequestBody RoomUserRequest request){
+        roomService.ban(user, request);
+        return ResponseEntity.ok().body(BaseResponseBody.of(200 ,"Success"));
+    }
+
+    @PostMapping("out/{room_seq}")
+    ResponseEntity<? extends BaseResponseBody> out(@Auth User user, @PathParam("room_seq") Long room_seq){
+        roomService.out(user, room_seq);
+        return ResponseEntity.ok().body(BaseResponseBody.of(200, "Success"));
+    }
+
+    @GetMapping("/disappear/{room_seq}")
+    ResponseEntity<RoomUserResponse> dissapear(@PathParam("room_seq") Long room_seq){
+        RoomUserResponse response = roomService.disappear(room_seq);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{room_seq}")
+    ResponseEntity<? extends BaseResponseBody> delete(@PathParam("room_seq") Long room_seq){
+        roomService.deleteRoom(room_seq);
+        return ResponseEntity.ok().body(BaseResponseBody.of(200, "Success"));
     }
 }
--- backend/src/main/java/com/ssafy/api/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/room/service/RoomServiceImpl.java
@@ -14,6 +14,7 @@ import com.ssafy.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -117,6 +118,4 @@ public class RoomServiceImpl implements RoomService{
         room.removeAllUser();
         roomRepository.deleteById(room_seq);
     }
-
-
 }
