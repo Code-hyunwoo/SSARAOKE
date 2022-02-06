7f017bff839009c25ccaefc4971bbfe18fc8afc8
bruno <bruno95@naver.com>
2022-02-06 17:37:14 +0900

    [FE]refactor(seungwon): DoorList에 Door배열 완료 & CreateRoom버튼 수정 완료

--- frontend/src/components/lobby/Door.js
+++ frontend/src/components/lobby/Door.js
@@ -1,7 +1,7 @@
 import Styles from "./Door.module.css";
 import { Link } from "react-router-dom";
 
-function Door({ title, user, thumbnail }) {
+function Door({ thumbnail, title, hostname, usercnt = 0 }) {
   return (
     <div className={Styles.roomsamecss}>
       <img className={Styles.video} src={thumbnail} alt="thumbnail" />
@@ -9,8 +9,8 @@ function Door({ title, user, thumbnail }) {
       <div className={Styles.roomtitle}>{title}</div>
 
       <div className={Styles.roomgroup}>
-        <div className={Styles.roomuser}>{user}</div>
-        <div className={Styles.roomfull}>8/8</div>
+        <div className={Styles.roomuser}>{hostname}</div>
+        <div className={Styles.roomfull}>{usercnt}/8</div>
       </div>
 
       <div className={Styles.tag}> #2010 #K-POP #발라드 #ROCK </div>
--- frontend/src/components/lobby/DoorList.js
+++ frontend/src/components/lobby/DoorList.js
@@ -9,79 +9,41 @@ function DoorList() {
     <>
       <CreateRoom />
       <div className={styles.gridcontainer}>
-        <div className={styles.griditem}>
-          <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-          <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-        </div>
+        <Door />
+        <Door />
       </div>
-
-      {/* <CreateRoom />
-      <Container className={styles.gridcontainer}>
-        <Row className={styles.griditem}>
-          <Col>
-            <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-          </Col>
-          <Col>
-            <Door title="90년대생 모여라" user="SSAFY2" thumbnail={duet} />
-          </Col>
-        </Row>
-        <Row style={{ paddingTop: "40vh" }}>
-          <Col>
-            <Door title="00년대생 모여라" user="SSAFY3" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 4 }}>
-            <Door title="10년대생 모여라" user="SSAFY4" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 6 }}>
-            <Door title="20년대생 모여라" user="SSAFY5" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 10 }}>
-            <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-          </Col>
-        </Row>
-        <Row style={{ paddingTop: "40vh" }}>
-          <Col>
-            <Door title="00년대생 모여라" user="SSAFY3" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 4 }}>
-            <Door title="10년대생 모여라" user="SSAFY4" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 6 }}>
-            <Door title="20년대생 모여라" user="SSAFY5" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 10 }}>
-            <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-          </Col>
-        </Row>
-        <Row style={{ paddingTop: "40vh" }}>
-          <Col>
-            <Door title="00년대생 모여라" user="SSAFY3" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 4 }}>
-            <Door title="10년대생 모여라" user="SSAFY4" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 6 }}>
-            <Door title="20년대생 모여라" user="SSAFY5" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 10 }}>
-            <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-          </Col>
-        </Row>
-        <Row style={{ paddingTop: "40vh" }}>
-          <Col>
-            <Door title="00년대생 모여라" user="SSAFY3" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 4 }}>
-            <Door title="10년대생 모여라" user="SSAFY4" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 6 }}>
-            <Door title="20년대생 모여라" user="SSAFY5" thumbnail={duet} />
-          </Col>
-          <Col md={{ offset: 10 }}>
-            <Door title="80년대생 모여라" user="SSAFY" thumbnail={duet} />
-          </Col>
-        </Row>
-      </Container> */}
+      <div className={styles.gridcontainer}>
+        <Door />
+        <div className={styles.griditem}>
+          <Door />
+          <Door />
+        </div>
+        <Door />
+      </div>
+      <div className={styles.gridcontainer}>
+        <Door />
+        <div className={styles.griditem}>
+          <Door />
+          <Door />
+        </div>
+        <Door />
+      </div>
+      <div className={styles.gridcontainer}>
+        <Door />
+        <div className={styles.griditem}>
+          <Door />
+          <Door />
+        </div>
+        <Door />
+      </div>
+      <div className={styles.gridcontainer}>
+        <Door />
+        <div className={styles.griditem}>
+          <Door />
+          <Door />
+        </div>
+        <Door />
+      </div>
     </>
   );
 }
