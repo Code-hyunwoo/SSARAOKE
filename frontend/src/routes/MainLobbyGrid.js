5e8515208fbbead7c19b83fe4cdb3c31bf3461a1
Code-hyunwoo <chichyun92@gmail.com>
2022-02-07 10:12:05 +0900

    [FE]feat: LobbyChat

--- frontend/src/routes/MainLobbyGrid.js
+++ frontend/src/routes/MainLobbyGrid.js
@@ -1,12 +1,14 @@
 import NavbarLobby from "../components/layout/NavbarLobby";
 import LobbyBackGround from "../components/lobby/background/LobbyBackGround";
 import DoorList from "../components/lobby/DoorList";
+import Lobbychat from "../components/lobby/Lobbychat";
 
 function MainLobbyGrid() {
   return (
     <div>
       <NavbarLobby />
       <LobbyBackGround />
+      <Lobbychat />
       <DoorList />
     </div>
   );
