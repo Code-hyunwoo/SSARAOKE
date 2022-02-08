b1d7337bd44a7a31e470b529368ed0549a4e5b64
Ubuntu <ubuntu@ip-172-26-4-116.ap-northeast-2.compute.internal>
2022-02-08 11:25:50 +0900

    [KU]Feat(signalling): add signalling server

---
+++ kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/GroupCallApp.java
@@ -0,0 +1,81 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.kurento.tutorial.groupcall;
+
+import org.kurento.client.KurentoClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+/**
+ *
+ * @author Ivan Gracia (izanmail@gmail.com)
+ * @since 4.3.1
+ */
+@SpringBootApplication
+@EnableWebSocket
+// /groupcall 경로에서 WebSocketHandler를 등록하기 위해 WebSocketConfigurer를 implements
+public class GroupCallApp implements WebSocketConfigurer {
+  // 이 코드상 현재 Kurento Media Server의 위치는 localhost의 TCP 8888 (변경가능)
+
+  /*
+    Single Page Application
+    Application Server - Client는 WebSocket으로 연결되어 있음 (req, res)
+   */
+
+  @Bean
+  public UserRegistry registry() {
+    return new UserRegistry();
+  }
+
+  @Bean
+  public RoomManager roomManager() {
+    return new RoomManager();
+  }
+
+  @Bean
+  public CallHandler groupCallHandler() {
+    return new CallHandler();
+  }
+
+  @Bean
+  public KurentoClient kurentoClient() {
+    return KurentoClient.create();
+  }
+
+  @Bean
+  public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+    container.setMaxTextMessageBufferSize(32768);
+    return container;
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(GroupCallApp.class, args);
+  }
+
+  @Override
+  // wss://i6a306.p.ssafy.io:8443/groupcall
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(groupCallHandler(), "/groupcall");
+  }
+}
