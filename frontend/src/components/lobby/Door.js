00c6753bc733ffcd8dd4357ecaa26530fa57b0cc
김혜란 <ss-android@naver.com>
2022-02-08 23:49:30 +0900

    [FE]feat(createroom, door): createroom, door API

--- frontend/src/components/lobby/Door.js
+++ frontend/src/components/lobby/Door.js
@@ -4,17 +4,23 @@ import { useState } from "react";
 import closedImg from "./img/closed2.PNG";
 
 function Door({ thumbnail = closedImg, title, hostname, usercnt = 0, tags }) {
+// function Door({roomdata}) {
   // 방 생성되면 created값 true로 변경
   const [created, setCreated] = useState(false);
 
   return (
     <div className={Styles.roomsamecss}>
+      {/* <img className={Styles.video} src={roomdata.thumbnail} alt="thumbnail" /> */}
+      {/* <img className={Styles.video} src={roomdata[6]} alt="thumbnail" /> */}
       <img className={Styles.video} src={thumbnail} alt="thumbnail" />
 
+      {/* <div className={Styles.roomtitle}>{roomdata[7]}</div> */}
       <div className={Styles.roomtitle}>{title}</div>
 
       <div className={Styles.roomgroup}>
+        {/* <div className={Styles.roomuser}>{roomdata[2]}</div> */}
         <div className={Styles.roomuser}>{hostname}</div>
+        {/* <div className={Styles.roomfull}>{roomdata[0]}/8</div> */}
         <div className={Styles.roomfull}>{usercnt}/8</div>
       </div>
 
--- frontend/src/components/lobby/DoorList.js
+++ frontend/src/components/lobby/DoorList.js
@@ -10,47 +10,27 @@ function DoorList() {
   //여기로 값을 가져오면, 각 Door로 값이 자동으로 갈까?
   
   //백에서 값 받아오기
+  const [roomdata, setRoomdata] = useState([]);
+  
   const Roomdata = async () => {
-    const [roomdata, setRoomdata] = useState([]);
-
-    useEffect(() => { 
+    // useEffect(() => { 
       axios
         .get('http://i6a306.p.ssafy.io:8080/api/v1/lobby')
         .then((response) => {
-          console.log(response);
+          console.log(response.data);
           setRoomdata(response.data);
+          console.log("룸데이타:", roomdata);
+          console.log("룸데이타1:", roomdata[0]);
         })
         .catch( e => {
           console.error(e);
         });
-    }, []);
-  }
+    };
+    // , []);
+  // }
 
 //태그 값
   const tags = []; //arrcheckedTags
-
-  //방 생성 값 받아오기
-  // const roomInfo =  async () => {
-  //   axios
-  //   .get('http://i6a306.p.ssafy.io:8080/api/v1/lobby')
-  //   .then(({data}) => {
-  //     console.log(data);
-  //     setState({
-  //       roomnum: data.room_seq, //방번호
-  //       title: data.title, //방제
-  //       hostname: data.owner_nicname, //방장
-  //       tags: data.tagList, //태그
-  //       isPrivate: data.isPrivate, //잠금여부
-  //       usercnt: data.current //현재 인원수
-  //     });
-  //     console.log(tags);
-  //   })
-  //   .catch(e => {
-  //     console.error(e);
-  //   });
-  // };
-
-  //
 
 
   return (
@@ -58,7 +38,7 @@ function DoorList() {
     <button onClick={Roomdata}>button</button>
       <CreateRoom />
       <div className={styles.gridcontainer}>
-        <Door tags={tags} roomdata={Roomdata}/>
+        <Door tags={tags} roomdata={roomdata[0]}/>
         <Door />
       </div>
       <div className={styles.gridcontainer}>
