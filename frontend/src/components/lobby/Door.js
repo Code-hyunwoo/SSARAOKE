6cc3846d7ada71b52c9309aef89cd87f8100e838
김혜란 <ss-android@naver.com>
2022-02-08 18:33:37 +0900

    test

--- frontend/src/components/lobby/Door.js
+++ frontend/src/components/lobby/Door.js
@@ -1,11 +1,27 @@
 import Styles from "./Door.module.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import closedImg from "./img/closed2.PNG";
+import axios from "axios";
 
 function Door({ thumbnail = closedImg, title, hostname, usercnt = 0, tags }) {
+// function Door({roomdata}) {
   // 방 생성되면 created값 true로 변경
   const [created, setCreated] = useState(false);
+
+  // const Roomdata = () => {
+  //   const [roomdata, setRoomdata] = useState([]);
+
+  //   useEffect(() => { 
+  //     axios.get('http://i6a306.p.ssafy.io:8080/api/v1/lobby')
+  //          .then(response => {
+  //           setRoomdata(response.data);
+  //           console.log(response.data);
+  //          });
+  //   }, []);
+  // }
+
+  //방 입장시 일단 베이직으로? 베이직으로 들어갔는데 다른 모드면? 방 생성할때 mode값도 같이 넘겨줘야하나
 
   return (
     <div className={Styles.roomsamecss}>
