59944235ec49cc6b64adb72981a644fcaf273edd
김혜란 <ss-android@naver.com>
2022-02-08 18:51:39 +0900

    [FE]feat: createRoom

--- frontend/src/components/lobby/DoorList.js
+++ frontend/src/components/lobby/DoorList.js
@@ -1,16 +1,64 @@
 import Door from "./Door";
 import styles from "./DoorList.module.css";
 import CreateRoom from "./CreateRoom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function DoorList() {
   // Door.js에서 map쓸거면 선택한 태그(tags)를 배열로 받아와야함
   // OR 객체로 받아와서 배열로 변환
-  const tags = [];
+  //여기로 값을 가져오면, 각 Door로 값이 자동으로 갈까?
+  
+  //백에서 값 받아오기
+  const Roomdata = async () => {
+    const [roomdata, setRoomdata] = useState([]);
+
+    useEffect(() => { 
+      axios
+        .get('http://i6a306.p.ssafy.io:8080/api/v1/lobby')
+        .then((response) => {
+          console.log(response);
+          setRoomdata(response.data);
+        })
+        .catch( e => {
+          console.error(e);
+        });
+    }, []);
+  }
+
+//태그 값
+  const tags = []; //arrcheckedTags
+
+  //방 생성 값 받아오기
+  // const roomInfo =  async () => {
+  //   axios
+  //   .get('http://i6a306.p.ssafy.io:8080/api/v1/lobby')
+  //   .then(({data}) => {
+  //     console.log(data);
+  //     setState({
+  //       roomnum: data.room_seq, //방번호
+  //       title: data.title, //방제
+  //       hostname: data.owner_nicname, //방장
+  //       tags: data.tagList, //태그
+  //       isPrivate: data.isPrivate, //잠금여부
+  //       usercnt: data.current //현재 인원수
+  //     });
+  //     console.log(tags);
+  //   })
+  //   .catch(e => {
+  //     console.error(e);
+  //   });
+  // };
+
+  //
+
+
   return (
     <>
+    <button onClick={Roomdata}>button</button>
       <CreateRoom />
       <div className={styles.gridcontainer}>
-        <Door tags={tags} />
+        <Door tags={tags} roomdata={Roomdata}/>
         <Door />
       </div>
       <div className={styles.gridcontainer}>
