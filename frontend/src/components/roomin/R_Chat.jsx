fd079e70804fec268d5032201cfbcdcf32bdd2dc
Code-hyunwoo <chichyun92@gmail.com>
2022-02-09 10:25:59 +0900

    [FE]feat: Basic, RoomChat fix

--- frontend/src/components/roomin/R_Chat.jsx
+++ frontend/src/components/roomin/R_Chat.jsx
@@ -1,15 +1,48 @@
 import styles from "./Room.module.css";
+import ReactScrollableFeed from "react-scrollable-feed"
+import { connect } from "react-redux";  
 
 
-function RoomChat ({mode}){
-        
+function RoomChat ({mode, Nickname, sendChat, chatArr}){
+
+    const onKeyPress=(e)=> {
+        if(e.key ==='Enter'){
+        const msg = e.target.value
+          sendChat(msg);
+          e.target.value = ''
+        }
+      };
+
+
     return (
         <div className={styles.rgb}>
-            <input id={mode} type="text" placeholder="Chat"></input>
+            <div id={mode}>
+                <div className={styles.ChatBox}>
+                    <ReactScrollableFeed>
+                    {chatArr.map((ele) => (
+                    <div className={styles.Chat}>
+                        <span className={styles.ChatName}>{ele.name} :</span>
+                        <span className={styles.ChatContent}>{ele.msg}</span>
+                    </div>
+                     ))}
+                    </ReactScrollableFeed>
+                </div>
+                <div className={styles.InputBox}>
+                    <div className={styles.Nick}>{Nickname}</div>
+                    <input className={styles.txtmsg} type={"text"} id={"chatTxt"} onKeyPress={onKeyPress} placeholder='메세지'></input>
+                
+                </div>
+
+            </div>
         </div>
     )
 
 }
 
 
-export default RoomChat
+function mapStateToProps(state) {
+    const Nickname = state[0].nickname 
+    return { Nickname };
+  }
+
+export default connect(mapStateToProps, null)(RoomChat);
