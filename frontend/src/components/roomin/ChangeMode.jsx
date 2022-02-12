b3cc8a6e08bcbd2b00884ffbd87727f9b60467ab
Code-hyunwoo <chichyun92@gmail.com>
2022-02-12 23:13:54 +0900

    [FE]feat: fix merging

--- frontend/src/components/roomin/ChangeMode.jsx
+++ frontend/src/components/roomin/ChangeMode.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Button.module.css";
-import { Link } from "react-router-dom";
 
 function ChangeMode({ closeChangeMode,transformBasic,transformSolo,transformDuet,transformFree }) {
   return (
@@ -10,20 +9,6 @@ function ChangeMode({ closeChangeMode,transformBasic,transformSolo,transformDuet
           <span>Select Mode</span>
         </div>
         <div className={styles.body}>
-<<<<<<< HEAD
-          <Link to="/basic" id={styles.modeLink}>
-            <button id={styles.modeButton}> Basic </button>
-          </Link>
-          <Link to="/free" id={styles.modeLink}>
-            <button id={styles.modeButton}> Free </button>
-          </Link>
-          <Link to="/solo" id={styles.modeLink}>
-            <button id={styles.modeButton}> Solo </button>
-          </Link>
-          <Link to="/duet" id={styles.modeLink}>
-            <button id={styles.modeButton}> duet </button>
-          </Link>
-=======
           <button id={styles.modeButton} onClick={transformBasic}>
               Basic
           </button>
@@ -36,7 +21,6 @@ function ChangeMode({ closeChangeMode,transformBasic,transformSolo,transformDuet
           <button id={styles.modeButton} onClick={transformDuet}>
               duet
           </button>
->>>>>>> front
         </div>
         <div className={styles.footer}>
           <button id={styles.backButton} onClick={() => closeChangeMode(false)}>
