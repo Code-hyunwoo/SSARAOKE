4c4c6eb3627b9af1abb2aec3dd5256a9474ea27a
정구아 <wjdrndk96@gmail.com>
2022-02-15 00:46:45 +0900

    [BE]refac(reservation): remove PREFIX, POSTFIX

--- backend/src/main/java/com/ssafy/api/reservation/service/ReservationServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/reservation/service/ReservationServiceImpl.java
@@ -5,6 +5,7 @@ import com.ssafy.api.reservation.dto.request.ReservationDeleteRequest;
 import com.ssafy.api.reservation.dto.response.ReservationResponse;
 import com.ssafy.common.exception.CustomException;
 import com.ssafy.common.exception.ErrorCode;
+import com.ssafy.common.util.JwtTokenProvider;
 import com.ssafy.domain.reservation.entity.Reservation;
 import com.ssafy.domain.reservation.repository.ReservationRepository;
 import com.ssafy.domain.room.entity.Room;
@@ -21,6 +22,8 @@ public class ReservationServiceImpl implements ReservationService {
 
     private final ReservationRepository reservationRepository;
     private final RoomRepository roomRepository;
+    public static final String TJ_PREFIX = "TJ노래방";
+    public static final String TJ_POSTFIX = " / TJ Karaoke";
 
     @Transactional
     @Override
@@ -28,10 +31,14 @@ public class ReservationServiceImpl implements ReservationService {
         //db에 리퀘스트로 받은 데이터를 넣어준다.
         Room room = roomRepository.findById(reservationAddRequest.getRoom_seq())
                 .orElseThrow(()->new CustomException(ErrorCode.ROOM_NOT_FOUND));
+        String newTitle = reservationAddRequest.getTitle().replace(TJ_PREFIX, "");
+        newTitle = newTitle.replace(TJ_POSTFIX, "");
+        newTitle = newTitle.replace("[]", "");  //[TJ노래방]이었다면 []만 남아있을 테니까 삭제
+        newTitle = newTitle.replace("/", "");
         Reservation reservation = Reservation.builder()
                 .room(room)
                 .song_no(reservationAddRequest.getSong_no())
-                .title(reservationAddRequest.getTitle())
+                .title(newTitle)
                 .build();
         reservationRepository.save(reservation);
         return getReservationList(room.getSeq());
